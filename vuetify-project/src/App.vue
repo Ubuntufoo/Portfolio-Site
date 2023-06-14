2779ce19d6a3de1268e8f4e317e60aa84e4dadd0
Timothy Murphy <TimothyMurphy123@gmail.com>
2023-06-13 20:55:30 -0400

    added sidebar

--- vuetify-project/src/App.vue
+++ vuetify-project/src/App.vue
@@ -1,5 +1,26 @@
 <template>
   <v-app>
-    
+    <SideBar />
+    <v-main>
+      <!-- cards -->
+    </v-main>
   </v-app>
 </template>
+
+<script>
+import { defineComponent } from 'vue';
+import SideBar from '@/components/SideBar.vue';
+
+export default defineComponent({
+  components: {
+    SideBar
+  },
+  setup() {
+    // Your setup code here
+
+    return {};
+  }
+});
+</script>
+
+
---
+++ vuetify-project/src/components/SideBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-navigation-drawer expand-on-hover rail>
+    Hello
+  </v-navigation-drawer>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    // Your setup code here
+  }
+});
+</script>
