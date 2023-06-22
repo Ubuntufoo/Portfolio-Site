529e1f041509d6f57aa0e86f571b0dc00e4b8416
Timothy Murphy <TimothyMurphy123@gmail.com>
2023-06-21 21:00:28 -0400

    nav text sizeup

--- vuetify-project/src/components/AppBar.vue
+++ vuetify-project/src/components/AppBar.vue
@@ -1,28 +1,28 @@
 <template>
-  <v-app-bar app dark :height="80" class="bg-transparent elevation-0">
+  <v-app-bar app dark :height="85" class="bg-transparent elevation-0">
     <v-app-bar-nav-icon @click="emitEvent"></v-app-bar-nav-icon>
     <div class="w-75 mx-auto">
       <v-list nav class="d-flex flex-nowrap justify-space-around mx-auto bg-transparent overflow-visible pt-16">
         <a href="#page1" class="text-decoration-none text-black">
-          <v-list-item title="Origin">
+          <v-list-item title="" class="text-h5 font-weight-bold">Origin
           </v-list-item>
         </a>
         <v-divider :thickness="2" vertical>
         </v-divider>
         <a href="#page2" class="text-decoration-none text-black">
-          <v-list-item title="Skills">
+          <v-list-item title="" class="text-h5 font-weight-bold">Skills
           </v-list-item>
         </a>
         <v-divider :thickness="2" vertical>
         </v-divider>
         <a href="#page3" class="text-decoration-none text-black">
-          <v-list-item title="Projects">
+          <v-list-item title="" class="text-h5 font-weight-bold">Projects
           </v-list-item>
         </a>
         <v-divider :thickness="2" vertical>
         </v-divider>
         <a href="#page4" class="text-decoration-none text-black">
-          <v-list-item title="Roadmap">
+          <v-list-item title="" class="text-h5 font-weight-bold">Roadmap
           </v-list-item>
         </a>
       </v-list>
