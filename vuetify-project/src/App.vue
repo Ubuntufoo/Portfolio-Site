8059a1e4dc9dd9f41dbdc79fbe960ff2820b0e6a
Timothy Murphy <Timmurphy123@gmail.com>
2023-06-24 17:07:47 -0400

    navigation complete

--- vuetify-project/src/App.vue
+++ vuetify-project/src/App.vue
@@ -5,30 +5,39 @@
     <NavSideBar :drawer="drawer" />
     <full-page :options="options" id="fullpage" ref="fullpage">
       <section class="section angledEl" data-anchor="page1">
-        <div class="slide compass">
-          <v-main>
-            <div class="chevron-left"></div>
-            <div class="chevron-right"></div>
-            <div class="chevron-down"></div>
-            <h1>page 1</h1>
-          </v-main>
-        </div>
-        <div class="slide compass">
+        <div class="slide">
           <v-main>
 
-            <h1>1.1</h1>
           </v-main>
         </div>
-        <div class="slide compass">
+        <div class="slide active">
+          <v-main>
+            <div class="arrow-left">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div class="arrow-right">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div class="arrow-down">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </v-main>
+        </div>
+        <div class="slide">
           <v-main>
 
-            <h1>1.2</h1>
           </v-main>
         </div>
       </section>
-      <section class="section angledEl compass" data-anchor="page2">
+      <section class="section angledEl" data-anchor="page2">
         <v-main>
-          <h1>2</h1>
+
         </v-main>
       </section>
       <!-- <section class="section angledEl" data-anchor="page3">
@@ -66,9 +75,8 @@ export default defineComponent({
         navigation: true,
         controlArrows: false,
         scrollingSpeed: 1500,
-
         anchors: ['page1', 'page2', 'page3', 'page4'],
-        sectionsColor: ['gray', 'gray', , ,]
+        sectionsColor: ['#cdcbcc', '#cdcbcc']
       }
     };
   },
@@ -76,7 +84,6 @@ export default defineComponent({
   }
 });
 </script>
-
 
 <style scoped>
 /* .angledEl {
@@ -87,43 +94,7 @@ export default defineComponent({
   background-color: #dbdbdb;
   clip-path: polygon(50% 0, 0% 50%, 100% 50%, 50% 100%, 0 50%);
 } */
-.chevron-left {
-  width: 0;
-  height: 0;
-  border-top: 50px solid transparent;
-  border-right: 50px solid #9b0909;
-  /* Change color as desired */
-  border-bottom: 50px solid transparent;
-  position: fixed;
-  top: 50%;
-  left: 0;
-  transform: translateY(-50%);
-}
 
-.chevron-right {
-  margin-left: auto;
-  width: 0;
-  height: 0;
-  border-top: 50px solid transparent;
-  border-right: 50px solid #9b0909;
-  /* Change color as desired */
-  border-bottom: 50px solid transparent;
-  top: 50%;
-  left: 0;
-  transform: translateY(-50%);
-}
-
-.chevron-down {
-  margin: auto;
-  width: 0;
-  height: 0;
-  border-top: 50px solid transparent;
-  border-left: 50px solid #000000;
-  /* Change color as desired */
-  border-bottom: 50px solid transparent;
-  top: 50%;
-  right: 0;
-  transform: translateY(-50%);
-}
+/* Left nav chevrons */
 </style>
 
--- vuetify-project/src/components/AppBar.vue
+++ vuetify-project/src/components/AppBar.vue
@@ -1,28 +1,28 @@
 <template>
-  <v-app-bar app dark :height="85" class="bg-transparent elevation-0">
-    <v-app-bar-nav-icon @click="emitEvent" class="d-sm-none"></v-app-bar-nav-icon>
-    <div class="w-75 mx-auto d-none d-sm-block">
-      <v-list nav class="d-flex flex-nowrap justify-space-around mx-auto bg-transparent overflow-visible pt-16">
+  <!-- revert app bar background to transparent when done designing -->
+
+  <v-app-bar app dark :height="140" class="bg-transparent elevation-0">
+    <v-app-bar-nav-icon @click="emitEvent" class="mb-auto"></v-app-bar-nav-icon>
+    <div class="w-50 mx-auto d-none d-sm-block mt-auto">
+      <v-list nav class="d-flex flex-wrap justify-space-around bg-transparent mt-4">
         <a href="#page1" class="text-decoration-none text-black">
-          <v-list-item title="" class="text-h5 font-weight-bold">Origin
+          <v-list-item class="text-h5">Skills
           </v-list-item>
         </a>
         <v-divider :thickness="2" vertical>
         </v-divider>
         <a href="#page1/1" class="text-decoration-none text-black">
-          <v-list-item title="" class="text-h5 font-weight-bold">Skills
+          <v-list-item class="text-h5 font-weight-bold">Origin
           </v-list-item>
         </a>
         <v-divider :thickness="2" vertical>
         </v-divider>
         <a href="#page1/2" class="text-decoration-none text-black">
-          <v-list-item title="" class="text-h5 font-weight-bold">Projects
+          <v-list-item class="text-h5">Projects
           </v-list-item>
         </a>
-        <v-divider :thickness="2" vertical>
-        </v-divider>
-        <a href="#page2" class="text-decoration-none text-black">
-          <v-list-item title="" class="text-h5 font-weight-bold">Roadmap
+        <a href="#page2" class="text-decoration-none text-black flex-1-1-100 text-center mr-8 mt-md-8">
+          <v-list-item class="text-h5">Roadmap
           </v-list-item>
         </a>
       </v-list>
@@ -45,3 +45,7 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+
+</style>
